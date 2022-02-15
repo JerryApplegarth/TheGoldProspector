c7fb7f75943aed8198b4e2aa2da933e612713957
Jerry Applegarth <applegarth1630@gmail.com>
2022-02-14 17:12:42 -0800

    6th commit-1.6 view model complete

--- app/src/main/java/com/fourapplecompose/thegoldprospector/viewModel/GoldViewModel.kt
+++ app/src/main/java/com/fourapplecompose/thegoldprospector/viewModel/GoldViewModel.kt
@@ -1,4 +1,29 @@
 package com.fourapplecompose.thegoldprospector.viewModel
 
-class GoldViewModel {
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.fourapplecompose.thegoldprospector.model.Gold
+import com.fourapplecompose.thegoldprospector.network.ApiService
+import kotlinx.coroutines.launch
+
+class GoldViewModel: ViewModel() {
+
+    var goldListResponce:List<Gold> by mutableStateOf(listOf())
+    var errorMessage: String by mutableStateOf("")
+
+    fun getGoldList(){
+        viewModelScope.launch {
+            val apiService = ApiService.getInstance()
+            try {
+                val goldList = apiService.getGold()
+                goldListResponce = goldList
+            }
+            catch (e:Exception){
+                errorMessage = e.message.toString()
+            }
+        }
+    }
 }
